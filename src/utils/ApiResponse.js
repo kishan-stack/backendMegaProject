f44737f0ebf3ca1d0114e759a7447fb1691ae245
kishan <varmajaykishan63@gmail.com>
2024-07-09 00:53:15 +0530

    fix apiresponse status issue

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,7 +3,7 @@ class ApiResponse {
         this.statusCode = statusCode;
         this.message = message;
         this.data = data;
-        this.success = statusCode > 400 ;
+        this.success = statusCode < 400 ;
     }
 }
 
