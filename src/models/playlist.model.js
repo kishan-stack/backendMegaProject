b7f02999601e1cbba84fce9dfc9079373d3d111f
kishan <varmajaykishan63@gmail.com>
2024-07-12 19:14:59 +0530

    add comment, like, playlist, tweet model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    descripton:{
+        type:String,
+        required:true
+    },
+    videos:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"video"
+        }
+    ],
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true});
+
+export const Playlist = mongoose.model("Playlist",playlistSchema);
